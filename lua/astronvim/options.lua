7d14db10111be6bc98724fa74c22f7d42d19442b
tian <istiancz@gmail.com>
2024-07-09 00:32:27 +0800

    feat: disable codelens

--- lua/astronvim/options.lua
+++ lua/astronvim/options.lua
@@ -54,7 +54,7 @@ local options = astronvim.user_opts("options", {
     autoformat_enabled = true, -- enable or disable auto formatting at start (lsp.formatting.format_on_save must be enabled)
     autopairs_enabled = true, -- enable autopairs at start
     cmp_enabled = true, -- enable completion at start
-    codelens_enabled = true, -- enable or disable automatic codelens refreshing for lsp that support it
+    codelens_enabled = false, -- enable or disable automatic codelens refreshing for lsp that support it
     diagnostics_mode = 3, -- set the visibility of diagnostics in the UI (0=off, 1=only show in status line, 2=virtual text off, 3=all on)
     highlighturl_enabled = true, -- highlight URLs by default
     icons_enabled = true, -- disable icons in the UI (disable if no nerd font is available)
