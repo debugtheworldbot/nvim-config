27c206ca4a68655c73a6d4a92d66d85933a75cdd
tian <istiancz@gmail.com>
2024-07-24 16:57:51 +0800

    chore:update

--- init.lua
+++ init.lua
@@ -18,12 +18,6 @@ if vim.g.vscode then
   vim.keymap.set({ "n", "v" }, "H", "<Cmd>Tabprevious<CR>")
   vim.keymap.set({ "n", "v" }, "L", "<Cmd>Tabnext<CR>")
 else
-  for _, source in ipairs {
-    -- "astronvim.mappings",
-  } do
-    local status_ok, fault = pcall(require, source)
-    if not status_ok then vim.api.nvim_err_writeln("Failed to load " .. source .. "\n\n" .. fault) end
-  end
   if astronvim.default_colorscheme then
     if not pcall(vim.cmd.colorscheme, astronvim.default_colorscheme) then
       require("astronvim.utils").notify(
